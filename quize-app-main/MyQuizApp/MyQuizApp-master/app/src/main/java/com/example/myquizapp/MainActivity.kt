577a29a8e5e30cfc38c217ba812c3fa39d251507
ST10183958 <128601617+ST10183958@users.noreply.github.com>
2023-09-22 20:55:00 +0200

    Add files via upload

---
+++ quize-app-main/MyQuizApp/MyQuizApp-master/app/src/main/java/com/example/myquizapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myquizapp
+
+// Reference's
+//https://github.com/JARNBOY/MyQuizApp/tree/master
+//https://www.youtube.com/watch?v=b21fiIyOW4A&t=2s
+//https://www.youtube.com/watch?v=pXZR0QiwvrU
+
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val etName: EditText = findViewById(R.id.et_name)
+        val btnStart: Button = findViewById(R.id.btn_start)
+
+
+        btnStart.setOnClickListener {
+            if (etName.text.isNotEmpty()) {
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, etName.text.toString())
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this,"Please enter your name",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
